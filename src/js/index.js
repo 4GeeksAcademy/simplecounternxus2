259da2e3369174861b5dbbaead57eb5497d9576d
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-08-03 23:07:11 +0000

    cambios finales

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,44 @@
-//import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+function SimpleCounter() {
+    const [counter, setCounter] = useState(0);
 
-//import your own components
-import Home from "./component/home.jsx";
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(prevCounter => prevCounter + 1);
+        }, 1000);
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+        return () => clearInterval(interval); // Limpiar el intervalo al desmontar
+    }, []);
 
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+
+    const bodyStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        margin: 0,
+        padding: 0,
+        minHeight: '100vh'
+    };
+
+    return (
+        <div style={bodyStyle}>
+        <div className=" d-flex bg-black justify-content-center fs-5 text-white bigCounter">
+            <div className="calendar fs-1 me-2">
+                <i className="far fa-clock"></i>
+            </div>
+            <div className="four fs-1 me-2">{four % 10}</div>
+            <div className="three fs-1 me-2">{three % 10}</div>
+            <div className="two fs-1 me-2">{two % 10}</div>
+            <div className="one fs-1 me-2">{one % 10}</div>
+        </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#app'));
+root.render(<SimpleCounter />);
